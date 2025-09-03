340de71fdd239833da9e2454ea4ab01b369303bf
sainikm <isainikkhan@gmail.com>
2025-09-03 21:21:52 +0530

    Add config.go to manage reading and writing to json cnfig file

---
+++ internal/config/config.go
@@ -0,0 +1,69 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func (cfg *Config) SetUser(userName string) error {
+	cfg.CurrentUserName = userName
+	return write(*cfg)
+}
+
+func Read() (Config, error) {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, fmt.Errorf("failed to get config file path: %w", err)
+	}
+
+	file, err := os.Open(configFilePath)
+	if err != nil {
+		return Config{}, fmt.Errorf("failed to open config file: %w", err)
+	}
+
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return Config{}, fmt.Errorf("failed to decode config JSON: %w", err)
+	}
+
+	return cfg, nil
+}
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("failed to get user home directory: %w", err)
+	}
+	return filepath.Join(homeDir, configFileName), nil
+}
+
+func write(cfg Config) error {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(configFilePath)
+	if err != nil {
+		return fmt.Errorf("failed to create config file: %w", err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(cfg); err != nil {
+		return fmt.Errorf("failed to encode config to JSON: %w", err)
+	}
+
+	return nil
+}
