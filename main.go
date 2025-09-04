51393ae362811002d3f4ba8bafa74c0182c03104
sainikm <isainikkhan@gmail.com>
2025-09-05 02:39:17 +0530

    Update main

--- main.go
+++ main.go
@@ -2,26 +2,34 @@ package main
 
 import (
 	"fmt"
-	"log"
-
 	"github.com/sainikmandal/gator/internal/config"
+	"os"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatalf("failed to read config: %v", err)
+		fmt.Println("failed to read config:", err)
+		os.Exit(1)
+	}
+	s := &state{cfg: &cfg}
+
+	cmds := &commands{registeredCommands: make(map[string]func(*state, command) error)}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Println("not enough arguments")
+		os.Exit(1)
 	}
 
-	if err := cfg.SetUser("sainik"); err != nil {
-		log.Fatalf("failed to update user: %v", err)
-	}
-
-	// Read back updated config to verify
-	cfg, err = config.Read()
+	cmd := command{Name: os.Args[1], Args: os.Args[2:]}
+	err = cmds.run(s, cmd)
 	if err != nil {
-		log.Fatalf("failed to read updated config: %v", err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
-
-	fmt.Println("Current User:", cfg.CurrentUserName)
 }
