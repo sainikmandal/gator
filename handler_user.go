99121b3b09ffbb69825e2e8d48193f2fadd38ed1
sainikm <isainikkhan@gmail.com>
2025-09-05 08:00:57 +0530

    Add registration handler and update login handler with db lookup

--- handler_user.go
+++ handler_user.go
@@ -1,19 +1,64 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/google/uuid"
+	"github.com/sainikmandal/gator/internal/database"
+	"os"
+	"time"
 )
 
 func handlerLogin(s *state, cmd command) error {
-	if len(cmd.Args) != 1 {
+	if len(cmd.Args) < 1 {
 		return fmt.Errorf("username is required")
 	}
 	name := cmd.Args[0]
 
-	err := s.cfg.SetUser(name)
+	_, err := s.db.GetUserByName(context.Background(), name)
+	if err != nil {
+		fmt.Println("user does not exists: ", name)
+		os.Exit(1)
+	}
+
+	err = s.cfg.SetUser(name)
 	if err != nil {
 		return fmt.Errorf("couldn't set current user: %w", err)
 	}
 	fmt.Println("Logged in as:", name)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("username is required for registration")
+	}
+	name := cmd.Args[0]
+
+	_, err := s.db.GetUserByName(context.Background(), name)
+	if err == nil {
+		fmt.Println("user with that name already exists")
+		os.Exit(1)
+	}
+
+	params := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+	}
+
+	user, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		return fmt.Errorf("failed to register user: %w", err)
+	}
+
+	if err := s.cfg.SetUser(user.Name); err != nil {
+		return fmt.Errorf("failed to set current user: %w", err)
+	}
+
+	fmt.Printf("User registered successfully:\nID: %s\nCreatedAt: %s\nUpdatedAt: %s\nName: %s\n",
+		user.ID, user.CreatedAt, user.UpdatedAt, user.Name)
+
 	return nil
 }
