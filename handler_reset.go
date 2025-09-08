8b2806c3f14efa7b3c43f0bd6844e0c372aa450f
sainikm <isainikkhan@gmail.com>
2025-09-09 03:40:13 +0530

    Edit args

--- handler_reset.go
+++ handler_reset.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func handlerReset(s *state, _ command) error {
+func handlerReset(s *state, cmd command) error {
 	err := s.db.ResetUsers(context.Background())
 	if err == nil {
 		fmt.Println("reset successful!")
